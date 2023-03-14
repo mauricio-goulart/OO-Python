de63eaf1dbb5383aac14ccd619ce3d79307689e4
scarlet-project <mauricioazevedogou@gmail.com>
2023-03-14 18:31:47 -0300

    Update aula01.py

--- aula01/aula01.py
+++ aula01/aula01.py
@@ -1,14 +1,8 @@
 class Fila:
-    fila = []
-
+    def __init__(self):
+        self.fila = []
     def entrar(self, nome):
         self.fila.append(nome)
     def sair(self):
         self.fila.pop(0)
 
-supermercado = Fila()
-
-supermercado.entrar('Mauricio')
-
-print(supermercado.fila)
-print(len(supermercado.fila))
